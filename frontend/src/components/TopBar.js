b4a1696d340b7952ef1100f7be06330f468f0ad9
inzam01 <shamasmarty@gmail.com>
2025-08-26 17:38:12 +0530

    Update TopBar.js

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -9,8 +9,8 @@ const TopBar = () => {
 
   // fetch topbar data from backend
   useEffect(() => {
-    fetch("http://localhost:5000/api/topbar")
-      // fetch("https://back1-sp50.onrender.com")
+    // fetch("http://localhost:5000/api/topbar")
+      fetch("https://back1-sp50.onrender.com/api/topbar")
       .then(response => response.json())
       .then(data => setTopBarData(data))
       .catch(error => console.error("Error fetching topbar data:", error));
